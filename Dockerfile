440a78b75cc856591b4fd98a320162e6b7662ddd
kowjeg <kowj@yandex.ru>
2025-04-20 21:17:28 +0300

    fix fat jar build (spring boot maven plugin)

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM openjdk:23-jdk-slim
 
 WORKDIR /app
 
-
-
-COPY target/TGBotCoolGuyOfTheDay-1.0-SNAPSHOT-shaded.jar /app/TGBotCoolGuyOfTheDay.jar
+COPY target/TGBotCoolGuyOfTheDay-1.0-SNAPSHOT.jar /app/TGBotCoolGuyOfTheDay.jar
 
 ENTRYPOINT ["java", "-jar", "TGBotCoolGuyOfTheDay.jar"]
--- src/main/java/ru/saveldu/Main.java
+++ src/main/java/ru/saveldu/Main.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication()
-@EntityScan(basePackages = "ru.saveldu.entities")
+@SpringBootApplication(scanBasePackages = "ru.saveldu")
 @EnableJpaRepositories(basePackages = "ru.saveldu.repositories")
+@EntityScan(basePackages = "ru.saveldu.entities")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
